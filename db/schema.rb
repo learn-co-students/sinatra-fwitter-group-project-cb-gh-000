d4231f87744135ab0b2dfac122b534f96858382f
HugoGao <tiancaigaohua@163.com>
2017-02-08 19:22:47 +0800

    feat: finish basic funtion

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,9 +15,8 @@ ActiveRecord::Schema.define(version: 20170207151213) do
   create_table "tweets", force: :cascade do |t|
     t.string  "content"
     t.integer "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
-
-  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string "username"
