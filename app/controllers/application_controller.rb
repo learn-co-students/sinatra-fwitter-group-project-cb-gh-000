3382d51f56bb6b7b758fb6620cbaabfcfd585eb0
Robert Wayne <robertjwayne@gmail.com>
2017-05-15 19:46:14 -0400

    Build login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -67,9 +67,22 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/login' do
+    if logged_in?
+      redirect to '/tweets'
+    else
+      erb :'/users/login'
+    end
   end
 
   post '/login' do
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
   end
 
   get '/logout' do
