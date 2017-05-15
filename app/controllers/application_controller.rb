28ef33016dc3f36a46906ad8e3ff25fddd1e7f0f
Robert Wayne <robertjwayne@gmail.com>
2017-05-15 15:56:09 -0400

    Create logged_in? and current_user helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,4 +50,15 @@ class ApplicationController < Sinatra::Base
 
   get '/logout' do
   end
+
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
