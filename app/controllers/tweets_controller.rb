80c6ea3863d15a6b366890b45c3a52a216e8d1e8
Aurangzaib <aurangzaib.danial@gmail.com>
2017-08-09 16:19:21 +0500

    complete tweet edit feature

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,10 +31,33 @@ class TweetsController < ApplicationController
   get '/tweets/:id' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
-      @user = current_user
       erb :'tweets/show_tweet'
     else
       redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      @user = current_user
+      if @user.id == @tweet.user_id
+        erb :'tweets/edit_tweet'
+      else
+        redirect to '/tweets'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if @tweet.update(content: params[:content])
+      redirect to "/tweets/#{@tweet.id}"
+    else
+      flash[:message] = 'Enter some content!'
+      redirect to "/tweets/#{params[:id]}/edit"
     end
   end
 end
