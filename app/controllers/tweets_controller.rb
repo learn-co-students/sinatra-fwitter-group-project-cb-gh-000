79b429ad3c3f74b954e50fbfd2488aa5d46f721b
Quinn Dizon <qdizon@gmail.com>
2018-08-22 21:12:23 +0000

    Working...

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,8 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    @user = User.find_by_id(session[:user_id])
+    erb :'tweets/index'
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,47 @@
 class UsersController < ApplicationController
 
+  get '/' do
+    'Welcome to Fwitter'
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'user/signup'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] != "" && params[:email] != "" && params[:password] != ""
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'user/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    @user = user.authenticate(params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    end
+  end
+
+  #Helper Methods
+  def logged_in?
+    !!session[:user_id]
+  end
 
 end
